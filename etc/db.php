c4797ae2db85fd3c68a62d8f25a34ac3e72b3d4c
Thorben Auer <thorrogramm@gmail.com>
2021-01-21 14:27:40 +0100

    Delete db.php

--- etc/db.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Create connection remove ".example" from name
-
-$db = new mysqli("localhost", "sae", "sae2021!","sae");
-if (!$db) {
-    die("Connection failed: " . mysqli_connect_error());
-}
-?>
